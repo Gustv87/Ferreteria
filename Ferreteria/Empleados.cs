4ead8bd04d07161fdbe0969f7d02530c0ebd7620
GMartinez <tavomartinez87@gmail.com>
2018-06-11 19:56:03 -0600

    Se creo el proyecto de ferreteria para la clase teoria de base de datos

---
+++ Ferreteria/Empleados.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ferreteria
+{
+    public partial class Empleados : Form
+    {
+        public Empleados()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Empleados_Load(object sender, EventArgs e)
+        {
+            Form1 cl = new Form1();
+            cl.Show();
+
+            
+        }
+    }
+}
---
+++ Ferreteria/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using Oracle.ManagedDataAccess.Client;
+
+namespace Ferreteria
+{
+    public partial class Form1 : Form
+    {
+        OracleConnection con = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Empleados nue = new Empleados();
+
+            nue.Show();
+
+            this.Hide();
+        }
+
+        private void coneccionBd()
+        {
+            String connectionString = ConfigurationManager.ConnectionStrings["ferreDB"].ConnectionString;
+            con = new OracleConnection(connectionString);
+            try
+            {
+                con.Open();
+            }
+            catch (Exception exp) { }
+
+
+        }
+
+    }
+}
